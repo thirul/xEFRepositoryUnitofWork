0dace657360ec352335a7ad1ee5fa3b45b73a6d4
Thiru Sudagoni <2thiru@gmail.com>
2013-05-22 16:02:04 +0100

    sql script for creation of tables added

---
+++ xEFRepositoryUnitofWork.Data/SQL_Scripts/tables_creation_script.sql
@@ -0,0 +1,45 @@
+﻿
+
+/****** Object:  Table [dbo].[Category]    Script Date: 05/22/2013 15:58:21 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Category](
+	[CategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[UpdateDate] [datetime] NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK__Category__7F60ED59] PRIMARY KEY CLUSTERED 
+(
+	[CategoryId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+/****** Object:  Table [dbo].[Product]    Script Date: 05/22/2013 15:58:31 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[ProductId] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+	[ShortDescription] [nvarchar](512) NOT NULL,
+	[Price] [float] NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[UpdateDate] [datetime] NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK__Product__7D78A4E7] PRIMARY KEY CLUSTERED 
+(
+	[ProductId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK__Product__Categor__00551192] FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[Category] ([CategoryId])
+GO
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK__Product__Categor__00551192]
